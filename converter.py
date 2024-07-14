f2623aa621416944099dd24a7a9a2d9a7bcda007
Kacper <kgrzesik17@gmail.com>
2024-07-14 17:09:12 +0200

    dodano obsluge bledow zwiazanych z nieoblsugiwanym formatem pliku

--- converter.py
+++ converter.py
@@ -7,7 +7,10 @@ def main():
         file1 = sys.argv[1]
         file2 = sys.argv[2]
 
-        window(file1, file2)
+        if check_extension(file1) and check_extension(file2):
+            window(file1, file2)
+        else:
+            error_window(2)
         
     except IndexError:
         error_window(1)
@@ -25,7 +28,6 @@ def check_extension(file):
             return True
     
     return False
-
 
 
 def clicked():
@@ -70,11 +72,17 @@ def error_window(error_id):
 
     match error_id:
         case 1:
-            error_message = "Error: no arguments found"
-            error_solution = "You have to run the file from the command line and pass it 2 arguments."
+            error_message = "Error 1: No arguments found"
+            error_description = "You have to run the file from the command line and pass it 2 arguments."
             error_example = "Example: converter.exe [file.extension] [file.expected_extension]"
+        case 2:
+            error_message = "Error 2: Extension not supported"
+            error_description = "You can only pass files that match the supported extensions"
+            error_example = "Supported extensions: .xml, .json, .yml, .yaml"
         case _:
-            error_message = "Error: unknown error"
+            error_message = "An unknown error occured"
+            error_description = "No description available."
+            error_example = "No example available."
 
     error_message_label = QtWidgets.QLabel(win)
     error_message_label.setText(error_message)
@@ -82,10 +90,10 @@ def error_window(error_id):
     error_message_label.adjustSize()
 
     
-    error_solution_label = QtWidgets.QLabel(win)
-    error_solution_label.setText(error_solution)
-    error_solution_label.move(50, 100)
-    error_solution_label.adjustSize()
+    error_description_label = QtWidgets.QLabel(win)
+    error_description_label.setText(error_description)
+    error_description_label.move(50, 100)
+    error_description_label.adjustSize()
 
     error_example_label = QtWidgets.QLabel(win)
     error_example_label.setText(error_example)
@@ -94,3 +102,6 @@ def error_window(error_id):
 
     win.show()
     sys.exit(app.exec_())
+
+
+main()
