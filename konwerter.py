a2c46064a5ee84a99ddc90211c014b0340f04894
Kacper <kgrzesik17@gmail.com>
2024-07-14 15:44:12 +0200

    dodanie podstawowych elementow UI

--- konwerter.py
+++ konwerter.py
@@ -2,11 +2,22 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+def clicked():
+    print("test")
+
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
+    win.setGeometry(800, 400, 300, 300)
     win.setWindowTitle("Konwerter")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("My first label")
+    label.move(50, 50)
+
+    b1 = QtWidgets.QPushButton(win)
+    b1.setText("przycisk")
+    b1.clicked.connect(clicked)
 
     win.show()
     sys.exit(app.exec_())
